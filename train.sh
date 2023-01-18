2ce700d7a60368eb3e1e202a0e7e06fb73c77557
ZombaSY <tjxhwl@gmail.com>
2023-01-18 03:00:48 +0000

    update some hyper-parameters

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1 python train.py --img 512 --batch 32 --epochs 300 --data hair_detection.yaml --weights pretrained/yolov5s.pt
+CUDA_VISIBLE_DEVICES=0,1 python train.py --img 512 --batch 32 --epochs 3000 --data hair_detection.yaml --weights pretrained/yolov5s.pt
